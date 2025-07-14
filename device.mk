77655d1baa8a348c93ccdd595e72d01ac400d597
Jooyung Han <jooyung@google.com>
2025-07-14 11:13:32 +0530

    tanzanite: Install apex for clearkey service
    Bug: 329737888
    Test: local build
    Change-Id: Ibf5574ce7da0b927ef8179663bb03a039955bcd3

--- device.mk
+++ device.mk
@@ -132,7 +132,7 @@ PRODUCT_PACKAGES += \
 
 # DRM (Clearkey)
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    com.android.hardware.drm.clearkey
 
 # FastbootD
 PRODUCT_PACKAGES += \
