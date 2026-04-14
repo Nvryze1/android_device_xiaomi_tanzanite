695a7d17a871b16651f51054b204d11766645687
Nvryze1 <navrayzejr@gmail.com>
2026-04-14 18:54:58 +0700

    Update and rename lineage_tanzanite.mk to navy_tanzanite.mk

--- navy_tanzanite.mk
+++ navy_tanzanite.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/navyos/config/common_full_phone.mk)
 
 # Inherit from tanzanite device
 $(call inherit-product, device/xiaomi/tanzanite/device.mk)
@@ -26,7 +26,7 @@ USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := tanzanite
-PRODUCT_NAME := lineage_tanzanite
+PRODUCT_NAME := navy_tanzanite
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 24117RN76O
 PRODUCT_MANUFACTURER := xiaomi
@@ -40,3 +40,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=Redmi/tanzanite_id/tanzanite:15/AP3A.240905.015.A2/OS2.0.213.0.VOGEUXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+
